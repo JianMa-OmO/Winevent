a76b09ac6f0d0da170e5b75593568f8cd0e33d33
JianMa-OmO <zyq_is_me@outlook.com>
2025-04-06 21:13:55 +0800

    Update Main_calculate.py

--- Main_calculate.py
+++ Main_calculate.py
@@ -9,7 +9,6 @@ import os
 
 
 w = wmi.WMI()
-activatecommand = ["slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX","slmgr /skms kms.loli.best","slmgr /ato"]
 
 # 获取Windows版本号，如10.0.26100
 def Windows_v():
@@ -19,51 +18,47 @@ def Windows_v():
 def Windows_e():
     return platform.release()
 
-# 获取电脑制造商名称，如Thunder
+# 获取电脑制造商名称，如Thunderobot
 def computer_manufacturer():
     return w.Win32_ComputerSystem()[0].Manufacturer
 
-
+# 获取电脑型号，如Thunderbook16
 def computer_model():
     return w.Win32_ComputerSystem()[0].Model
 
+# 获取处理器名称，如Intel64 Famliy Model 186 setpping 2
 def processor_name():
     return platform.processor()
 
-
+# 获取处理器架构，如64bit
 def processor_acchitecture():
     return list(platform.architecture())
 
-
+# 获取主板序列号，如0929WE00J303
 def mainboard_n():
     return ''.join(mbn.SerialNumber.strip() for mbn in w.Win32_BaseBoard())
 
-
+# 获取bios序列号，如JT900001UKIS
 def bios_n():
     return ''.join(biosn.SerialNumber.strip() for biosn in w.Win32_BIOS())
 
-
+# 获取硬盘序列号，如38TG_2ISE_20QQ_02DE
 def hard_n():
     return ''.join(hardn.SerialNumber for hardn in w.Win32_DiskDrive())
 
 def vermessage():
     messagebox.showinfo(title="版本", message="版本v1.1.4。")
 
-
+# 清理休眠文件（需要管理员权限）
 def cleanpowercfg():
     import os
     os.system("powercfg -h off")
     messagebox.showinfo(title="提示", message="清除文件已关闭。")
 
-def actiave():
-    for a in activatecommand:
-        os.system(a)
-
 # 下载函数
 def download(whatdownload):
     __all_download = {"python" : "https://mirrors.huaweicloud.com/python/3.13.2/python-3.13.2.exe",
-                      "jdk" : "https://repo.huaweicloud.com:8443/artifactory/java-local/jdk/13+33/jdk-13_windows-x64_bin.exe",
-                      "000-激活-win11" : lambda : actiave()}
+                      "jdk" : "https://repo.huaweicloud.com:8443/artifactory/java-local/jdk/13+33/jdk-13_windows-x64_bin.exe"}
     if whatdownload not in __all_download.keys():
         messagebox.showerror("下载","没有找到下载源。")
     elif not  whatdownload == "000-激活-win11":
